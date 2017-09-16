3b642b7c9f6fa2bd135653f4b16bca654932cd27
strangerintheq <tripmannn@gmail.com>
2017-09-16 23:14:17 +0300

    z-fighting shader bugfix

--- src/main/resources/shaders/curtain.fragment.glsl
+++ src/main/resources/shaders/curtain.fragment.glsl
@@ -1,4 +1,3 @@
-
 uniform sampler2D u_texture;
 
 uniform float cx;
@@ -6,14 +5,19 @@ uniform float angle;
 uniform float width;
 uniform float height;
 
-varying vec4 v_texCoord;
+varying vec4 tex;
 
 void main() {
-    if (v_texCoord.x < 0.) discard;
-    if (v_texCoord.y < 0.) discard;
-    if (v_texCoord.x > 1.) discard;
-    if (v_texCoord.y > 1.) discard;
-    float tan = tan(angle);
-    if (gl_FragCoord.x + tan*gl_FragCoord.y > cx*width + height*tan*.5) discard;
-    gl_FragColor = texture2D(u_texture, v_texCoord.xy);
+
+    gl_FragColor = texture2D(u_texture, tex.xy);
+
+    bool discardFragment =
+        // if fragment outside texture coordinates
+        tex.x < 0.0 || tex.x > 1.0 || tex.y < 0.0 || tex.y > 1.0 ||
+        // if fragment in right half of curtain
+        tan(angle)*(gl_FragCoord.y - height*.5) > cx*width - gl_FragCoord.x;
+
+    if (discardFragment) gl_FragColor.a = 0.0;
+    // todo mesure perfomance of this discard
+//    if (discardFragment) discard;
 }
